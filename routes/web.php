27424bf6e9fdaac2172e1077bef6fa22574b0168
iazaran <eazaran@gmail.com>
2020-08-20 09:13:32 +0300

    Basic web and api routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TutorController@index');
+Route::resource('tutors', 'TutorController')->except('show');
